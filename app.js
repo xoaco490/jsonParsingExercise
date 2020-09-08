bc1aee196f057e3b0c73997b20568127c7ff5f92
JoaquinF <49355700+xoaco490@users.noreply.github.com>
2020-09-07 21:42:46 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,23 +3,34 @@ const app = express();
 const https = require("https");
 
 app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=ApiKey&units=metric";
+    const query = "London";
+    const apiKey = "apikey";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units="+unit;
 
     https.get(url,function(response){
         console.log(response.statusCode);
-        
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            //console.log(weatherData);
-            const temp = weatherData.main.temp;
-            console.log(temp);
-            //[0] porque es un array con un solo item
-            const weatherDescription = weatherData.weather[0].description;
-            console.log(weatherDescription);
-            res.write("<p><h3>The weather is currently "+weatherDescription+"</h3></p>");
-            res.write("<h1>The temperature in london is "+temp+"C°</h1>");
-            res.send();
-        });
+        if(response.statusCode!=200){
+            res.send("<img src=https://http.cat/"+response.statusCode+">");
+        }else{
+            response.on("data", function(data){
+            
+            
+                const weatherData = JSON.parse(data);
+                //console.log(weatherData);
+                const temp = weatherData.main.temp;
+                console.log(temp);
+                //[0] porque es un array con un solo item
+                const weatherDescription = weatherData.weather[0].description;
+                console.log(weatherDescription);
+                const icon = weatherData.weather[0].icon;
+                const iconImageURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+                res.write("<p><h3>The weather is currently "+weatherDescription+"</h3></p>");
+                res.write("<h1>The temperature in london is "+temp+" degrees celsius</h1>");
+                res.write("<img src="+iconImageURL+">")
+                res.send();
+            });
+        }
     });
 });
 
